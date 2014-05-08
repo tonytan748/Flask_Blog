231852f931fd2f0e9cb074b51f41b9cd95cac5d2
tan yong <violin748@hotmail.com>
2014-05-08 10:03:20 +0000

    update

---
+++ config.py
@@ -0,0 +1,7 @@
+CSRF_ENABLED=True
+SECRET_KEY='this is i am many times to check the test but alives fialed'
+
+OPENID_PROVIDERS=[{'name':'Google','url':'https://www.google.con/accounts/o8/id'},
+		{'name':'Yahoo','url':'https://me.yahoo.com'},
+		{'name':'AOL','url':'https://openid.aol.com/<username>'},
+		{'name':'MyOpenID','url':'https://www.myopenid.com'}]
