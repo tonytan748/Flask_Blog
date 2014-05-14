70eb0d4b45756e6d2f36f26edd84975a61963890
tan yong <violin748@hotmail.com>
2014-05-14 08:45:57 +0000

    add new port

--- config.py
+++ config.py
@@ -8,7 +8,21 @@ SQLALCHEMY_MIGRATE_REPO=os.path.join(basedir,'db_repository')
 CSRF_ENABLED=True
 SECRET_KEY='this is i am many times to check the test but alives fialed'
 
-OPENID_PROVIDERS=[{'name':'Google','url':'https://www.google.con/accounts/o8/id'},
+OPENID_PROVIDERS=[{'name':'Google','url':'https://www.google.com/accounts/o8/id'},
 		{'name':'Yahoo','url':'https://me.yahoo.com'},
 		{'name':'AOL','url':'https://openid.aol.com/<username>'},
 		{'name':'MyOpenID','url':'https://www.myopenid.com'}]
+
+#mail server settings
+MAIL_SERVER='loaclhost'
+MAIL_PORT=25
+MAIL_USERNAME=None
+MAIL_PASSWORD=None
+
+#administrator list
+ADMINS=['tonytan748@gmail.com']
+
+#pageination
+POSTS_PER_PAGE=3
+
+WHOOSH_BASE = os.path.join(basedir,'search.db')
